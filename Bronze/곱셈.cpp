c09530c21133dd3e17c849d8608c8c0667515ef7
jangsumi <aboutimus@gmail.com>
2022-05-05 23:50:22 +0900

    220505

---
+++ Bronze/곱셈.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num1, num2;
+    int num3, num4, num5, num6;
+    int num2_1, num2_10, num2_100;
+
+    cin >> num1 >> num2;
+
+    num2_1 = num2 % 10;
+    num2_10 = (num2/10) % 10;
+    num2_100 = (num2/100) % 10;
+
+    //cout << num2_1 << " " << num2_10 << " " << num2_100 << endl;
+
+    num3 = num1 * num2_1;
+    num4 = num1 * num2_10;
+    num5 = num1 * num2_100;
+
+    num6 = num1 * num2;
+
+    cout << num3 << endl;
+    cout << num4 << endl;
+    cout << num5 << endl;
+    cout << num6 << endl;    
+}
